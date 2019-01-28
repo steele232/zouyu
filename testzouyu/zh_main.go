8ce7195ce241869d5f43f5ff4f1a47e2782fa997
Jonathan Steele <jonathan.steele232@gmail.com>
2019-01-28 16:01:26 -0700

    Updated Tests to use new keywords, especially for 'make' and 'function'.

--- testzouyu/zh_main.go
+++ testzouyu/zh_main.go
@@ -7,7 +7,7 @@
 	"fmt"
 )
 
-程序 主要() {
+函数 主要() {
 
 	循环 i:=0; i < 10; i++ {
 		fmt.Printf("第%v个\n", i+1)
@@ -15,17 +15,17 @@
 
 	fmt.Println("走语考试开始了")
 
-	项目 := 做([]整数, 3)
+	项目 := 初始化([]整数, 3)
 	循环 _, v := 范围(项目) {
 		fmt.Println(v)
 	}
 
-	走 程序() {
+	走 函数() {
 		fmt.Println("时间过得很慢")
 	}()
 	time.Sleep(2 *time.Second)
 
-	走 程序() {
+	走 函数() {
 		fmt.Println("时间过得很慢")
 	}()
 	time.Sleep(2 *time.Second)
